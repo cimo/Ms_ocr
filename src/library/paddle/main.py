b324b9a69ab5196f4f7cbb700b3671c4c6d68656
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-09-26 21:54:40 +0900

    update

--- src/library/paddle/main.py
+++ src/library/paddle/main.py
@@ -3,10 +3,10 @@ from paddleocr import LayoutDetection
 
 os.makedirs("/home/app/file/output/paddle/", exist_ok=True)
 
-pathModelBlockLayout = "/home/app/src/library/paddle/PP-DocBlockLayout/"
+pathModelBlockLayout = "/home/app/src/library/paddle/PP-DocLayout_plus-L/"
 pathImage = "/home/app/file/input/1_jp.jpg"
 
-modelBlockLayout = LayoutDetection(model_dir=pathModelBlockLayout, model_name="PP-DocBlockLayout")
+modelBlockLayout = LayoutDetection(model_dir=pathModelBlockLayout, model_name="PP-DocLayout_plus-L")
 outputBlockLayoutList = modelBlockLayout.predict(pathImage, batch_size=1)
 
 for outputBlockLayout in outputBlockLayoutList:
